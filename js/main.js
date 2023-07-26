917f1e98dbafaae44c687ad411842cf5598ad040
Camila Ricaud <cami.l.ricaud@gmail.com>
2023-07-25 22:26:48 -0300

    agregue el archivo javascript, cambie las etiquetas section de los header por etiquetas div

---
+++ js/main.js
@@ -0,0 +1,48 @@
+//comentario en una sola linea
+/* comentario
+multilinea*/
+
+/*declaracion y asignacion de variable correcta*/
+let edad=40; /* ES6*/
+let nombre = "daniel";
+let apellido = "juarez";
+let numero = 33;
+
+/*contantes, variable que nunca cambia su valor, hay que establecer su valor en la misma linea*/
+
+const PI = 3.14;
+
+/*concatenando variables*/
+let nombreCompleto= nombre + apellido;
+let resultado= PI + 3.24;
+resultado = PI + numero;
+
+/* metodo de salida*/
+console.log(apellido);
+
+alert ("hola tarola");
+
+/*metodo de entrada*/
+
+let usuario=prompt("ingresa tu nombre de usuario");
+
+let num1=prompt("ingresa un numero");
+num1= parseFloat(num1);
+let num2= parseFloat(prompt("ingresa otro numero"));
+let resultado2=num1 + num2;
+alert(resultado2);
+
+// condicionales
+
+let temperatura = 30;
+if (temperatura <= 30){
+    console.log("esta agradable");
+}else{
+    console.log("que calor!!");
+}
+
+if (temperatura > 30){
+    console.log("que calor!!");
+}
+
+
