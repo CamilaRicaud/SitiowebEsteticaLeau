4253c469d80034b974de43b33966389bb64b3459
camilaricaud <cami.l.ricaud@gmail.com>
2023-08-22 19:01:31 -0300

    realice un array de objetos y funciones de orden superior en carpeta js para segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,62 +1,70 @@
-//comentario en una sola linea
-/* comentario
-multilinea*/
+const tratamientoscorporales=[
+    {id: 1, nombre: "anticelulitico", precio: 10800},
+    {id: 2, nombre: "anticelulitico Reafirmante", precio: 13000},
+    {id:3, nombre: "tonificador", precio:7800},
+    {id:4, nombre: "reducto I", precio:12500},
+    {id:5, nombre: "reductor II", precio:12500},
+    {id:6, nombre: "reductor III", precio:12300},
+    {id:7, nombre: "piernas perfectas", precio:9000},
+    {id:8, nombre: "bikini express", precio:13000},
+    {id:9, nombre: "promo flash", precio:10000},
+    {id:10, nombre: "promo ondas rusas", precio:9200},
+    {id:11, nombre: "maderoterapia", precio:11500},
+    {id:12, nombre: "maderoterapia + vela slim plus", precio: 14900},
+    {id:13, nombre: "maderoterapia + ondas rusas/body up", precio: 14000},
+    {id:14, nombre: "drenaje linfatico", precio: 10500},
+    {id:15, nombre: "drenaje linfatico + ondas rusas/body up", precio: 13000},
+    {id:16, nombre: "drenaje linfatico + vela slim plus", precio: 14500},
+    {id:17, nombre: "pase libre 1", precio:18900},
+    {id:18, nombre: "pase libre 2", precio: 19800},
+];
 
-/*declaracion y asignacion de variable correcta*/
-let nombre= "daniel";
-let apellido= "perez";
-
-/*contantes, variable que nunca cambia su valor, hay que establecer su valor en la misma linea*/
-
-const PI = 3.14;
-
-/*concatenando variables*/
-let nombreCompleto= nombre + apellido;
-let resultado= PI + 3.24;
-
-/* metodo de salida*/
-console.log(apellido);
-
-alert ("hola tarola");
-
-/*metodo de entrada*/
-
-let usuario=prompt("ingresa tu nombre de usuario");
-
-let num1=prompt("ingresa un numero");
-num1= parseFloat(num1);
-let num2= parseFloat(prompt("ingresa otro numero"));
-let resultado2=num1 + num2;
-alert(resultado2);
-
-// condicionales
-
-let temperatura = 30;
-if (temperatura <= 30){
-    console.log("esta agradable");
-}else{
-    console.log("que calor!!");
-}
-/* signos: = asignacion, == comparacion, != distinto,*/
-
-if (temperatura > 30){
-    console.log("que calor!!");
-}else{
-    console.log("esta agradable");
+function saludo(nombre){
+    alert("Bienvenido/a: " + nombre);
 }
 
-let Usuario= prompt("Ingresa tu usuario");
-
-if (Usuario == "") {
-    console.log("no ingresaste tu usuario");
-}else{
-    console.log("Bienvenido/a" + Usuario);
+for (let i = 1; i<= 3; i++){
+    let nombrecompleto = prompt("Ingresa tu nombre y apellido");
+    if(nombrecompleto != ""){
+        saludo(nombrecompleto);
+        break;
+    }else {
+        alert("No ingresaste tu nombre, intentelo nuevamente.");
+    }
 }
 
-//otra forma de ponerlo
-if (Usuario != "") {
-    console.log("Bienvenido/a" + Usuario);
-}else{
-    console.log("no ingresaste tu usuario");
+function buscartratamiento(array,filtro){
+    const encontrado= array.find((el)=> el.nombre.includes(filtro));
+    return encontrado;
 }
+
+let ingresartratamiento = prompt("Ingresa el tratamiento que quieras buscar, para cancelar ingresar 0");
+
+const tratamientoencontrado = buscartratamiento(tratamientoscorporales,ingresartratamiento);
+
+do{
+    if (ingresartratamiento != "0" && ingresartratamiento!=""){
+        alert("Tratamiento encontrado: " + tratamientoencontrado.nombre + ", precio:$"+ tratamientoencontrado.precio);
+        break;
+    }else {
+        alert("La busqueda ha sido cancelada");
+    }
+}while (ingresartratamiento !="0" && ingresartratamiento!="");
+
+let ingresarprecio = prompt("Ingrese su presupuesto");
+
+function filtrarporprecio(array,filtro){
+    return array.filter(el=> el.precio<=filtro);
+}
+const preciotratamientos= filtrarporprecio(tratamientoscorporales, ingresarprecio);
+
+do{
+    if (isNaN(ingresarprecio)){
+        alert("Solo se pueden ingresar numeros");
+        break;
+    }else {
+        console.log(preciotratamientos);
+    }
+}while (isNaN(ingresarprecio));
+
 
